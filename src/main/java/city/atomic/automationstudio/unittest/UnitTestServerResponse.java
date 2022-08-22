b0164d0ff043a37bec671eb3fc25b15f3694dc88
Michael R. Taylor <michael@atomic.city>
2022-08-21 22:19:19 -0500

    Added class to represent JSON response from Automation Runtime unit test list response.

---
+++ src/main/java/city/atomic/automationstudio/unittest/UnitTestServerResponse.java
@@ -0,0 +1,45 @@
+package city.atomic.automationstudio.unittest;
+
+import java.util.List;
+
+public class UnitTestServerResponse {
+    public class UnitTest {
+        private String device;
+        private String description;
+
+        public String getDevice() {
+            return device;
+        }
+
+        public void setDevice(String device) {
+            this.device = device;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
+    private String version;
+    private List<UnitTest> itemList;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<UnitTest> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<UnitTest> itemList) {
+        this.itemList = itemList;
+    }
+}
